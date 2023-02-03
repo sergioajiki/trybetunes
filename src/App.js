15c10fbbbd95f5d3dd73a29c367732dcb94e1e87
Sergio <sergioajk@hotmail.com>
2023-02-03 17:17:56 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
+// import Album from './components/Album';
+// import Favorites from './components/Favorites';
+// import Login from './components/Login';
+// import NotFound from './components/NotFound';
+// import Profile from './components/Profile';
+// import ProfileEdit from './components/ProfileEdit';
+// import Search from './components/Search';
+import Routes from './components/Routes';
 
 class App extends React.Component {
   render() {
     return (
-      <Switch>
+      <>
         <p>My TrybeTunes</p>
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-      </Switch>
+        <Routes />
+      </>
     );
   }
 }
