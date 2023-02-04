fe2d70a20d136227be0dd7c10657bafafddd93e0
Sergio <sergioajk@hotmail.com>
2023-02-04 12:47:15 -0300

    req 5

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,54 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  state = {
+    isLogButtonDisabled: true,
+    artistName: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.validateButtonLogin());
+  };
+
+  validateButtonLogin = () => {
+    const { artistName } = this.state;
+    const minLength = 2;
+    const validButton = artistName.length < minLength;
+    this.setState({ isLogButtonDisabled: validButton });
+  };
+
   render() {
+    const { isLogButtonDisabled, artistName } = this.state;
+
     return (
       <div data-testid="page-search">
         Página do Search
         <Header />
+        <form>
+          <label htmlFor="artistName">
+            <input
+              data-testid="search-artist-input"
+              id="artistName"
+              name="artistName"
+              type="text"
+              value={ artistName }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ isLogButtonDisabled }
+            onClick={ () => console.log('Clicou no botão Procurar ') }
+          >
+            Procurar
+          </button>
+
+        </form>
+
       </div>
     );
   }
