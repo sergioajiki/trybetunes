61b49908466825d7f2059fabd5997a7feee81a4c
Sergio <sergioajk@hotmail.com>
2023-02-04 21:12:13 -0300

    req 7 2/3

--- src/components/Album.js
+++ src/components/Album.js
@@ -5,25 +5,77 @@ import getMusics from '../services/musicsAPI';
 // import MusicCard from './MusicCard';
 
 class Album extends React.Component {
+  state = {
+    infoAlbum: '',
+    isLoading: true,
+    listAlbumMusics: [],
+  };
+
+  componentDidMount() {
+    this.getMusicsFromAlbum();
+  }
+
   getMusicsFromAlbum = async () => {
     const { match: { params: { id } } } = this.props;
-    console.log({ id });
+    // console.log({ id });
     const listAlbumMusics = await getMusics(id);
-    console.log(listAlbumMusics);
+    this.setState({
+      infoAlbum: listAlbumMusics[0],
+      listAlbumMusics,
+      isLoading: false,
+    });
+    // console.log(listAlbumMusics);
   };
 
   render() {
-    const { match: { params: { id } } } = this.props;
-    console.log({ id });
-    console.log(this.getMusicsFromAlbum({ id }));
+    // const { match: { params: { id } } } = this.props;
+    const { isLoading, listAlbumMusics, infoAlbum } = this.state;
+    // console.log({ id });
+    console.log(listAlbumMusics);
+    // console.log(this.getMusicsFromAlbum({ id }));
+
+    if (isLoading) {
+      return <h2>Carregando...</h2>;
+    }
     return (
       <div data-testid="page-album">
         <Header />
         {/* <MusicCard /> */}
         Página do Album
+        <div>
+          <h2
+            data-testid="artist-name"
+          >
+            nome do artista aqui
+            { infoAlbum.artistName }
+          </h2>
+          <h2
+            data-testid="album-name"
+          >
+            nome do Album aqui
+            { infoAlbum.collectionName}
+          </h2>
+        </div>
 
-        <h2 data-testid="artist-name">nome do artista aqui</h2>
-        <h2 data-testid="album-name">nome do Album aqui</h2>
+        {/* {
+          listAlbumMusics.map((music, index) => (
+            <div key={ index }>
+              <h2
+                data-testid="artist-name"
+              >
+                nome do artista aqui
+                { music.artistName }
+              </h2>
+              <h2
+                data-testid="album-name"
+              >
+                nome do Album aqui
+                { music.collectionName}
+              </h2>
+            </div>
+          ))
+        } */}
+
       </div>
     );
   }
--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 // import Header from './Header';
-
+// import PropTypes from 'prop-types';
 class MusicCard extends React.Component {
   render() {
     // const { match: { params: { id } } } = this.props;
