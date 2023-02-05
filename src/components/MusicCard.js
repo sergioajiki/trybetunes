9fe116bb9549ab601a777692c0589f429a189f2d
Sergio <sergioajk@hotmail.com>
2023-02-05 13:25:41 -0300

    limpando consoles

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,17 +1,42 @@
 import React from 'react';
 // import Header from './Header';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  state = {
+    isLoading: false,
+    favoriteCheck: '',
+  };
+
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  favoriteSongChecked = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    await addSong();
+  };
+
   render() {
     const { albumSongList } = this.props;
+    const { isLoading, favoriteCheck } = this.state;
+    if (isLoading) {
+      return <h2>Carregando...</h2>;
+    }
     return (
       <div>
         Lista de musicas
         {
           albumSongList.map((song, index) => (
             <div key={ index }>
-              <p>
+              <span>
                 { song.trackName }
                 <audio data-testid="audio-component" src={ song.previewUrl } controls>
                   <track kind="captions" />
@@ -20,7 +45,17 @@ class MusicCard extends React.Component {
                   <code>audio</code>
                   .
                 </audio>
-              </p>
+              </span>
+              <label htmlFor="favoriteCheck">
+                <input
+                  data-testid={ `checkbox-music-${song.trackId}` }
+                  id="favoriteCheck"
+                  name="favoriteCheck"
+                  type="checkbox"
+                  checked={ this.favoriteSongChecked() }
+                  onChange={ this.onInputChange }
+                />
+              </label>
             </div>
           ))
         }
