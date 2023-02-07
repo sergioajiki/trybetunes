cc3c09148d3c5a623434baf80cf080007acab3ff
Sergio <sergioajk@hotmail.com>
2023-02-07 14:55:42 -0300

    req 12 iniciando, limpando comentarios

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,13 +1,72 @@
 import React from 'react';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Header from './Header';
+import MusicCard from './MusicCard';
 
 class Favorites extends React.Component {
+  state = {
+    isLoading: false,
+    // isFavorite: true,
+    listFavoriteSongs: [],
+  };
+
+  componentDidMount() {
+    this.loading();
+  }
+
+  componentDidUpdate() {
+    this.recoverFavoriteFromLocalStorage();
+    // this.loading();
+  }
+
+  recoverFavoriteFromLocalStorage = async () => {
+    const listFavoriteSongs = await getFavoriteSongs();
+    this.setState({
+      listFavoriteSongs,
+    });
+  };
+
+  loading = () => {
+    this.setState(
+      {
+        isLoading: true,
+      },
+      () => this.recoverFavoriteFromLocalStorage(),
+    );
+    this.recoverFavoriteFromLocalStorage();
+    this.setState({
+      isLoading: false,
+    });
+  };
+
   render() {
+    const { isLoading, listFavoriteSongs } = this.state;
+    if (isLoading) {
+      return <h2>Carregando...</h2>;
+    }
     return (
       <div data-testid="page-favorites">
         <Header />
         Página dos Favoritos
+        <div>
+          {
+            listFavoriteSongs.map((song) => (
+              <MusicCard
+                key={ song.trackId }
+                song={ song }
+                trackName={ song.trackName }
+                trackId={ song.trackId }
+                previewUrl={ song.previewUrl }
+              />
+            ))
+          }
+          {/* <MusicCard
+            albumSongList={ albumSongList }
+          /> */}
+        </div>
+
       </div>
+
     );
   }
 }
